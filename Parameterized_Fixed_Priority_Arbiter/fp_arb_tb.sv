1285ab86d544ad0512a50dbeac47d2ea3a27c0c7
UjvalMadhu <ujvalmadhu003@gmail.com>
2024-12-28 21:14:35 -0500

    Fixed Priority Arbiter

---
+++ Parameterized_Fixed_Priority_Arbiter/fp_arb_tb.sv
@@ -0,0 +1,38 @@
+///////////////////////////////////////////////////////////
+////  PARAMETERIZED FIXED PRIORITY ARBITER  TEST BENCH ////
+///////////////////////////////////////////////////////////
+// AUTHOR: Ujval Madhu
+// Created: 28 Dec 2024
+
+module fp_arb_tb;
+
+parameter num_ports = 5;
+parameter num_tests = 100;
+
+// Signals
+
+logic [num_ports-1:0] req_i;
+logic [num_ports-1:0] gnt_o;
+
+
+fp_arb #(
+    .num_ports(num_ports)
+) dut (
+    .req_i(req_i),
+    .gnt_o(gnt_o)
+);
+
+initial begin
+    
+    seed = $random;
+    $srandom(seed); // Seeds the pseudorandom number generator
+
+    // Random Request Generation
+    for(int i = 0; i < num_tests; i++) begin
+        req_i = {$random} % (1 << num_ports);
+        #10;
+        $display("Test %0d: req_i = %b, gnt_o = %b")
+    end
+
+end
+
