4d65fcf2e850c32ffe3d84838778f43397406bc6
UjvalMadhu <ujvalmadhu003@gmail.com>
2025-03-03 23:54:03 -0500

    Asynchronous Design

---
+++ Parameterized_Fixed_Priority_Arbiter/cons/results_conv_clocks_cons.tcl
@@ -0,0 +1,10 @@
+
+current_design $design_name
+
+create_clock -name clk -period $CLK_PERIOD -waveform [list 0 [expr $CLK_PERIOD / 2.0]] [get_ports clk]
+set_clock_uncertainty $CLK_SKEW clk
+
+current_design $design_name
+
+#/*********************************************************/
+#/*                                                       */
