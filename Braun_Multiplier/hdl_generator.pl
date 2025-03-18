c1dbb2a2566976fcda511e17669fe0104e0590aa
UjvalMadhu <ujvalmadhu003@gmail.com>
2025-03-17 21:49:11 -0400

    Braun Multiplier

--- Braun_Multiplier/hdl_generator.pl
+++ Braun_Multiplier/hdl_generator.pl
@@ -8,7 +8,7 @@
 #///   This Perl script generates Verilog code for a n bit Braun Multiplier       ///
 #///   based on user argument                                                     ///
 #///                                                                              ///
-#///   Copyright (C) 2025 Ujval Madhu,                                            ///                
+#///   Copyright (C) 2025 Ujval Madhu,                                            ///
 #///   This program is free software: you can redistribute it and/or modify       ///
 #///   it under the terms of the GNU General Public License as published by       ///
 #///   the Free Software Foundation, either version 3 of the License, or          ///
@@ -105,6 +105,11 @@ my $num_bits;
 my $outfile;
 my $help;
 my $rtl_dir = "rtl";
+# Variables for Time Format
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
+$year += 1900;  # localtime returns years since 1900
+$mon += 1;      # months are 0-based (0 = January)
+
 
 sub main{
 
@@ -135,12 +140,43 @@ sub main{
         $num_bits = 16;
     }
 
-    
+    #==============================================================
     # Creating a Full adder Module for use in the Braun Multiplier
+    #==============================================================
     
     open(my $fa_fh, '>', "rtl/fa.v") or die "Could not create verilog file for Full adder: $!";
 
     my $fa_code = <<"FA_CODE";
+////////////////////////////////////////////////////////////////////////////////////
+///                                                                              ///
+///                               Full Adder                                     ///
+///                                                                              ///
+////////////////////////////////////////////////////////////////////////////////////
+///   fa Module:  Full Adder Module                                              ///
+///                                                                              ///
+///   Copyright (C) $year Ujval Madhu,                                           ///
+///   This program is free software: you can redistribute it and/or modify       ///
+///   it under the terms of the GNU General Public License as published by       ///
+///   the Free Software Foundation, either version 3 of the License, or          ///
+///   (at your option) any later version.                                        ///
+///                                                                              ///
+///   This program is distributed in the hope that it will be useful,            ///
+///   but WITHOUT ANY WARRANTY; without even the implied warranty of             ///
+///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ///
+///   GNU General Public License for more details.                               ///
+///                                                                              ///
+///   You should have received a copy of the GNU General Public License          ///
+///   along with this program.  If not, see <https://www.gnu.org/licenses/>.     ///
+///                                                                              ///
+////////////////////////////////////////////////////////////////////////////////////
+//  CVS Log
+//
+//  Id: fa.v, v 1.0
+//
+//  \$Date: $year-$mon-$mday
+//  \$Revision: 1.0 $
+///  \$Author:  Ujval Madhu
+
 module fa(
     input a,
     input b,
@@ -165,27 +201,107 @@ FA_CODE
     close $fa_fh;
 
 
-# =====================================
-# Braun Multiplier Verilog Generation |
-#======================================
+# =================================================================
+#                Braun Multiplier Verilog Generation              |
+#==================================================================
 
     my ($module_name) = $outfile =~ /(\w+).v/;     # We need to use () for module_name to capture
     my $input_size  = int($num_bits/2); 
-    my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
-    $year += 1900;  # localtime returns years since 1900
-    $mon += 1;      # months are 0-based (0 = January)
+    
+    # Total number of AND gates and Full Adders
+    my $and_count = $input_size*$input_size;
+    my $fa_count  = $input_size*($input_size -1);
+
+    
+    # Partial Product Verilog Code Generation:
+    
+    my $ab = "";
+    my $pw_count = 0;        # Partial Product Wire Count;            
+    
+    for my $i (0..$input_size - 1){
+        
+        for my $j (0..$input_size - 1){
+        
+            $ab .= "and ab$j$i(ab_prod[$pw_count], a[$j], b[$i]);\n";
+            $pw_count +=1;
+        
+        }
+        
+        $ab .= "\n";
+    
+    }
+
+    
+    # Partial Sum Verilog Code Generation
+    
+    my $psum = "assign ps[0] = ab_prod[0];\n";
+    
+    for my $j (1..$input_size -1){     # 1 to input_size -1 rows of FAs 
+        
+        for my $k (1..$input_size){    # Input_size columns of FA
+            
+            if($j == 1){               # 1st Row of FAs have both operands = partial products
+
+                if($k == 1){           # 1st FA of the row has C_in = 1'b0          
+                    
+                    $psum .= "assign co[" . 8*($j-1)+ $k - 1 . " ] = 1'b0;\n";
+                    $psum .= "fa fa$j$k(.a(ab_prod[". 8*($j-1)+ $k ."]), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(co[" . 8*($j-1)+ $k - 1 . " ]), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(co[" . 8*($j-1)+ $k . " ]));\n"
+                
+                }elsif ($k == $input_size){  # last FA of the 1st row has 2nd operand = 1'b0 
+                    
+                    $psum .= "fa fa$j$k(.a(1'b0), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(co[" . 8*($j-1)+ $k - 1 . " ]), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(" . 8*($j-1)+ $k . " ));\n"
+                
+                }
+                else{                  # All other cases of 1st row
+
+                    $psum .= "fa fa$j$k(.a(ab_prod[" . 8*($j-1)+ $k . " ]), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(co[" . 8*($j-1)+ $k - 1 . " ]), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(co[" . 8*($j-1)+ $k . " ]));\n"
+                }
+
+            } 
+            else{                      # All other rows
+                if($k == 1){           # 1st FA of the row has C_in = 1'b0          
+                    
+                    $psum .= "fa fa$j$k(.a(ab_prod[" . 8*($j-1)+ $k . " ]), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(1'b0), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(co[" . 8*($j-1)+ $k . " ]));\n"
+                
+                }elsif ($k == $input_size){  # last FA of the row has 2nd operand =  c_out of last FA of previous row
+                    
+                    $psum .= "fa fa$j$k(.a(1'b0), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(co[" . 8*($j-1)+ $k - $input_size . " ]), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(" . 8*($j-1)+ $k . " ));\n"
+                
+                }
+                else{                  # All other cases of 1st row
+
+                    $psum .= "fa fa$j$k(.a(ab_prod[" . 8*($j-1)+ $k . " ]), .b(ab_prod[" . 8*($j-1) + $k + ($input_size - 1) . " ]), .c_in(co[" . 8*($j-1)+ $k - 1 . " ]), .sum(ps[" . 8*($j-1)+ $k . " ]), .c_out(co[" . 8*($j-1)+ $k . " ]));\n"
+                }
+            }
+        }
+    }
+
 
     open(my $bm_fh, '>', "rtl/$outfile") or die "Could not create verilog file for Braun Multiplier: $!";
 
     my $bm_code = <<"BM_CODE";
 ///////////////////////////////////////////////////////////////////////////////////
+///                                                                             ///
 ///                      $num_bits-Bit Braun Multiplier                                ///
 ///                                                                             ///
 ///////////////////////////////////////////////////////////////////////////////////
 ///   $module_name Module:  $num_bits-Bit Implementation of Braun Multiplier             ///
 ///                                                                             ///
-///   Copyright $year Ujval Madhu, All rights reserved                           ///
-///////////////////////////////////////////////////////////////////////////////////
+///   Copyright (C) $year Ujval Madhu,                                            ///
+///   This program is free software: you can redistribute it and/or modify       ///
+///   it under the terms of the GNU General Public License as published by       ///
+///   the Free Software Foundation, either version 3 of the License, or          ///
+///   (at your option) any later version.                                        ///
+///                                                                              ///
+///   This program is distributed in the hope that it will be useful,            ///
+///   but WITHOUT ANY WARRANTY; without even the implied warranty of             ///
+///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ///
+///   GNU General Public License for more details.                               ///
+///                                                                              ///
+///   You should have received a copy of the GNU General Public License          ///
+///   along with this program.  If not, see <https://www.gnu.org/licenses/>.     ///
+///                                                                              ///
+////////////////////////////////////////////////////////////////////////////////////
 //  CVS Log
 //
 //  Id: $outfile, v 1.0
@@ -197,9 +313,20 @@ FA_CODE
 module $module_name(
     input  [$input_size - 1 : 0] a,
     input  [$input_size - 1 : 0] b,
-    output [$input_size * 2 : 0] a,
-
+    output [$input_size * 2 : 0] prod
 );
+
+wire [$and_count - 1 : 0] ab_prod;          // For Partial Products
+wire [$fa_count - 1 : 0] ps;                // For Partial Sums generated by FAs
+wire [$fa_count - 1 : 0] co;                // For Carry_out from each FA      
+
+$ab
+
+@{[
+    "\n\n\n"
+]}
+
+$psum
 
 endmodule
 BM_CODE
