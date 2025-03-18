8f9de292e520993270c94837827330b07a30f860
UjvalMadhu <ujvalmadhu003@gmail.com>
2025-03-17 21:49:11 -0400

    Braun Multiplier

--- Braun_Multiplier/rtl/fa.v
+++ Braun_Multiplier/rtl/fa.v
@@ -1,3 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////////
+///                                                                              ///
+///                               Full Adder                                     ///
+///                                                                              ///
+////////////////////////////////////////////////////////////////////////////////////
+///   fa Module:  Full Adder Module                                              ///
+///                                                                              ///
+///   Copyright (C) 2025 Ujval Madhu,                                           ///
+///   This program is free software: you can redistribute it and/or modify       ///
+///   it under the terms of the GNU General Public License as published by       ///
+///   the Free Software Foundation, either version 3 of the License, or          ///
+///   (at your option) any later version.                                        ///
+///                                                                              ///
+///   This program is distributed in the hope that it will be useful,            ///
+///   but WITHOUT ANY WARRANTY; without even the implied warranty of             ///
+///   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              ///
+///   GNU General Public License for more details.                               ///
+///                                                                              ///
+///   You should have received a copy of the GNU General Public License          ///
+///   along with this program.  If not, see <https://www.gnu.org/licenses/>.     ///
+///                                                                              ///
+////////////////////////////////////////////////////////////////////////////////////
+//  CVS Log
+//
+//  Id: fa.v, v 1.0
+//
+//  $Date: 2025-3-17
+//  $Revision: 1.0 
+//  $Author:  Ujval Madhu
+
 module fa(
     input a,
     input b,
