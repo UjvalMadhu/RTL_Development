cf30d26d57592818a7b3c011bfe96898c51d77cd
UjvalMadhu <ujvalmadhu003@gmail.com>
2025-05-04 23:18:58 -0400

    Events to APB

--- Hands_on_RTL/Events_to_APB/rtl/events_to_APB.v
+++ Hands_on_RTL/Events_to_APB/rtl/events_to_APB.v
@@ -16,10 +16,20 @@
 //  $Author:  Ujval Madhu
 
 module events_to_APB(
-  input   logic        clk,
-  input   logic        reset
+  input wire  clk,
+  input wire  reset,
+  input wire  event_a_i,
+  input wire  event_b_i,
+  input wire  event_c_i,
 
+  output wire apb_sel_o,
+  output wire apb_penable_o,
+  output wire [31:0] apb_paddr_o,
+  output wire apb_pwrite_o,
+  output wire [31:0] apb_pwdata_o,
+  input  wire apb_pready_i
 );
+
 
 
 
